0cf36f4ac5948c77428a33475e80b6ca6e126620
iHyperDeveloper <46577493+iHyperDeveloper@users.noreply.github.com>
2019-01-10 23:36:01 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: Aktif, Komutlar yüklendi !!`);
+  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: ${client.user.username} ismi ile Sunucuya Giriş Yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setGame(`!öner <öneriniz> 🎉 `);
+  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: Şu an ` + client.channels.size + ` Adet Kanal Toplam ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() +  ` Kullanıcı Bulunmaktadır`);
+};
