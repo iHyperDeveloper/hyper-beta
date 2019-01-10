2e0ea6dad166dd0b93421a3e074d2a4184dd39e3
iHyperDeveloper <46577493+iHyperDeveloper@users.noreply.github.com>
2019-01-10 23:46:42 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: Aktif, Komutlar yüklendi !!`);
   console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: ${client.user.username} ismi ile Sunucuya Giriş Yapıldı!`);
-  client.user.setStatus("online");
+  client.user.setStatus("dnd");
   client.user.setGame(`!öner <öneriniz> 🎉 `);
   console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Yılmaz BOT: Şu an ` + client.channels.size + ` Adet Kanal Toplam ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() +  ` Kullanıcı Bulunmaktadır`);
